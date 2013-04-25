0d8a0c956864b9aeb7de958fd51bcba15ac5b9bd
Phil Nelson <phil@extrafuture.com>
2013-04-25 12:52:42 -0700

    Now is the time for action

--- main.lua
+++ main.lua
@@ -54,6 +54,7 @@ feelers = {
 }
 
 players = {}
+foods = {}
 
 time = love.timer.getTime( )
 
@@ -61,21 +62,22 @@ function chooseName()
 	return nameTableOne[ math.random( #nameTableOne ) ] ..  nameTableTwo[ math.random( #nameTableTwo ) ]
 end
 
-players[1] = {x = 20, y = 20, name = chooseName(), age = 18, feelers = feelers[ math.random( #feelers ) ]}
-players[2] = {x = 400, y = 400, name = chooseName(), age = 18, feelers = feelers[ math.random( #feelers ) ]}
+players[1] = {x = 20, y = 20, name = chooseName(), age = 18, feelers = feelers[ math.random( #feelers ) ], health = 10}
+players[2] = {x = 400, y = 400, name = chooseName(), age = 18, feelers = feelers[ math.random( #feelers ) ], health = 10}
 
 map = {}
 
 love.graphics.setMode( 800, 600 )
 
 stime = love.timer.getTime( )
+fstime = love.timer.getTime( )
 
 function love.draw()
 	
 	-- Draw Grid
 	local x = 1
 	local y = 1
-	love.graphics.setColor(0,0,255,90)
+	love.graphics.setColor(255,255,255,90)
     while x < 80 do
 		love.graphics.line(x*10,10,x*10,590)
 		x = x + 1
@@ -104,25 +106,60 @@ function love.draw()
 	end
 	
 	etime = love.timer.getTime( )
+	fetime = love.timer.getTime( )
 	
-	--if(etime-stime > 1) then
+	if(etime-stime > .5) then
 		-- Move guys
 		for key,value in pairs(players) do
-			if(players[1].x < players[2].x) then
-				players[1].x = players[1].x+.5
-			end
-	
-			if(players[1].y < players[2].y) then
-				players[1].y = players[1].y+.5
-			end
+			
+			local guyMove = moveGuy(players[key])
+			
+			players[key].x = guyMove.x
+			players[key].y = guyMove.y
 			
 			stime = love.timer.getTime( )
 		end
-		--end
+	end
+	
+	-- time to spawn some food?
+	if(fetime-fstime > 1) then
+		-- Make food
+		foods[#foods+1] = {kind = "apple", healthbuff = 1, ripeness = 0}
+		
+		fstime = love.timer.getTime( )
+	end
+	
+	-- Draw food
 	
 end
 
 function moveGuy(guy)
-	guy.y = y
-	guy.x = x
+	local moveIt = math.random(1,100)
+	local theMove = {x = guy.x, y = guy.y}
+	
+	print("moveIt for " .. guy.name .. " is " .. moveIt)
+	
+	if(moveIt > 0) then
+		-- just stand there
+	end
+	
+	if(moveIt > 20) then
+		theMove.x = guy.x-1
+	end
+	
+	if(moveIt > 30) then
+		theMove.y = guy.y+1
+	end
+	
+	if(moveIt > 40) then
+		theMove.x = guy.x+1
+	end
+	
+	if(moveIt > 50) then
+		theMove.y = guy.y-1
+	end
+	
+	print("theMove is " .. theMove.x .. "," .. theMove.y)
+	
+	return theMove
 end
